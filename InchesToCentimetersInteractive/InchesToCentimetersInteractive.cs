68c2a166604f668a570a4853348c082a3d8fbd65
Andres Arciniegas <andresarciniegas@Andress-Air.lan>
2025-09-10 23:29:56 -0400

    Adding Exersice 6 Ch2

---
+++ InchesToCentimetersInteractive/InchesToCentimetersInteractive.cs
@@ -0,0 +1,14 @@
+﻿namespace InchesToCentimetersInteractive;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Convert Inches to Centimeters");
+        Console.WriteLine("Please insert the amount of inches you want to convert");
+        string enter = Console.ReadLine();
+        double number = int.Parse(enter);
+        double result = number * 2.54;
+        Console.WriteLine($"The result is {result} cm");
+    }
+}
